9f42739300abb8847e97b169e2027a1d0a2ea473
Donghui Xu <donghui.xu@pnnl.gov>
2024-04-11 10:18:38 -0700

    add script to download and process land use and irrigate data

--- code/ISIMIP3a_Step02_Download_Land_Use_and_Irrigation.py
+++ code/ISIMIP3a_Step02_Download_Land_Use_and_Irrigation.py
@@ -2,9 +2,10 @@ import numpy as np
 import os
 from urllib.request import urlretrieve
 import netCDF4
+from create_nc import create_landuse_timeseries
 
-scenarios = ['1901soc','2015soc','histsoc']
-datasets  = ['15crops','urbanareas']
+scenarios = ['histsoc','1901soc','2015soc']
+datasets  = ['totals','urbanareas']
 
 if not os.path.exists('../data'):
 	os.mkdir('../data')
@@ -12,7 +13,9 @@ if not os.path.exists('../data/isimip3a'):
 	os.mkdir('../data/isimip3a')
 if not os.path.exists('../data/isimip3a/landuse_and_irrigation'):
 	os.mkdir('../data/isimip3a/landuse_and_irrigation')
-	
+
+
+# 1 Download the landuse data	
 for i in range(len(scenarios)):
     if not os.path.exists('../data/isimip3a/landuse_and_irrigation/'+scenarios[i]):
         os.mkdir('../data/isimip3a/landuse_and_irrigation/'+scenarios[i])
@@ -35,3 +38,32 @@ for i in range(len(scenarios)):
             url   = server + scenarios[i] + '/' + fname2
             print('Downloading ' + fname2)
             urlretrieve(url, '../data/isimip3a/landuse_and_irrigation/'+scenarios[i]+'/'+fname2)
+
+
+# 2 Process to landuse time series for histsoc
+fname1 = '../data/isimip3a/landuse_and_irrigation/histsoc/landuse-totals_histsoc_annual_1901_2021.nc'
+fname2 = '../data/isimip3a/landuse_and_irrigation/histsoc/landuse-urbanareas_histsoc_annual_1901_2021.nc'
+ncid1 = netCDF4.Dataset(fname1)
+ncid2 = netCDF4.Dataset(fname2)
+irrigated = np.flip(ncid1.variables['cropland_irrigated'][:].filled(fill_value=0),axis=1)
+rainfed   = np.flip(ncid1.variables['cropland_rainfed'][:].filled(fill_value=0),  axis=1)
+urbanarea = np.flip(ncid2.variables['urbanareas'][:].filled(fill_value=0),        axis=1)
+
+fin       = '/compyfs/inputdata/lnd/clm2/surfdata_map/landuse.timeseries_0.5x0.5_HIST_simyr1850-2015_c211019.nc'
+yr_start  = 1850
+yr_end    = 2015
+irrigated = irrigated * 100.0
+rainfed   = rainfed * 100.0
+urbanarea = urbanarea * 100.0
+irrigated = np.concatenate((irrigated[:51,:,:],irrigated[:115,:,:]),axis=0)
+rainfed   = np.concatenate((rainfed[:51,:,:]  ,rainfed[:115,:,:])  ,axis=0)
+
+create_landuse_timeseries(fin,irrigated,rainfed,urbanarea,yr_start,yr_end,scenarios[i])
+
+# Spinlim
+#for i in range(len(scenarios)):
+#    fname1 = 'landuse-totals_'     + scenarios[i] + '_annual_1901_2021.nc'
+#    fname2 = 'landuse-urbanareas_' + scenarios[i] + '_annual_1901_2021.nc'
+#    ncid1 = netCDF4.Dataset(fname1)
+#    ncid2 = netCDF4.Dataset(fname2)
+#    np.concatenate((np.repeat(np.reshape(irrigated[0,:,:],(1,360,720)),50,axis=0),irrigated),axis=0).shape
